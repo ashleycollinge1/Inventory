406a75134ecf8066654322704d1ebd630ab96dfb
Ashley Collinge <me@ashleycollinge.co.uk>
2020-10-04 16:21:00 +0100

    Added endpoint for returning list of devices

--- app/modules/mod_api/controllers.py
+++ app/modules/mod_api/controllers.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, jsonify
+from app import DB
+from app.modules.mod_api.models import Devices
 
 MOD_API = Blueprint('api', __name__, url_prefix='/api/', static_folder='static/mod_api', static_url_path='static')
 
@@ -7,7 +9,25 @@ def get_status():
     """
     Returns the status of the API.
 
-    Simple return saying 'success' for now
+    Simple return saying 'success' for now  
     """
     return jsonify({'ping': 'pong'})
+
+@MOD_API.route('/device/', methods=['GET'])
+def get_devices():
+    """
+    Returns a list of all devices formatted in JSON
+    """
+    devices = DB.session.query(Devices).all()
+    devices_json = []
+    if devices:
+        for device in devices:
+            device_json = {}
+            device_json['id'] = device.id
+            device_json['name'] = device.name
+            device_json['date_created'] = device.date_created
+            device_json['date_modified'] = device.date_modified
+            devices_json.append(device_json)
+    return jsonify(devices_json)
+
 
