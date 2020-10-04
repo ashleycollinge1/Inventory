85878d4242a7ab174537b353e4f7445e8fd39693
Ashley Collinge <me@ashleycollinge.co.uk>
2020-10-04 17:54:59 +0100

    Working on the frontend html/css

--- app/modules/mod_front/controllers.py
+++ app/modules/mod_front/controllers.py
@@ -1,13 +1,12 @@
 import os
 from flask import Blueprint, render_template, jsonify
 
-MOD_FRONT = Blueprint('front', __name__, url_prefix='/',
-                              static_folder='static/mod_agent', static_url_path='static')
+MOD_FRONT = Blueprint('front', __name__, url_prefix='/')
 
 @MOD_FRONT.route("/", methods=["GET"])
 def index():
     """
     Main dashboard
     """
-    return render_template('index.html')
+    return render_template('mod_front/index.html')
 
